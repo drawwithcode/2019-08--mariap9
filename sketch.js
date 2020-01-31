b8dfd2fc68d1a3862c5b00b52d1485bb236d3ecc
maria paula <mp.vargast79@gmail.com>
2020-01-31 00:41:46 -0500

    update corrections

--- sketch.js
+++ sketch.js
@@ -1,74 +1,87 @@
 var myMap;
 var canvas;
-var myPos;
+var myLoc;
+var data;
+var tLat = 34.169701;
+var tLon = 28.311039;
 
-var mappa = new Mappa('Leaflet');
-//
-var puppyLat = 4.635035;
-var puppyLng = -74.0724867;
-//
+var mappa = new Mappa("Mapbox", "pk.eyJ1IjoibWFyaWFwOTI1IiwiYSI6ImNrMnVrNGN5ajExNTMzYm56dDJ3b2JyaDIifQ.G5Wzsmz_LCuwx7Y6DOBOlQ");
+
 var options = {
-  lat: 0,
-  lng: 0,
-  zoom: 3,
-  style: "https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
-}
-
-var puppy = {
-  lat: puppyLat,
-  lng: puppyLng,
+  lat: tLat,
+  lng: tLon,
+  zoom: 5,
+  studio: true,
+  style: "mapbox://styles/mariap925/ck619aqyi06vs1ikbcim1n8l5",
 }
 
 function preload() {
-  myPos = getCurrentPosition();
+  // access json data with coordinates of world wonders
+  data = loadJSON("assets/wonders.json");
+  myLoc = getCurrentPosition();
 }
 
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
-
-  options.lat = myPos.latitude;
-  options.lng = myPos.longitude;
-
   myMap = mappa.tileMap(options);
-  myMap.overlay(canvas)
+  myMap.overlay(canvas);
+  console.log(data);
 
 
-  myMap.onChange(drawPoint);
 }
 
-function draw() {}
-
-function drawPoint() {
+function draw() {
   clear();
-  var disAway = calcGeoDistance(myPos.latitude, myPos.longitude, puppyLat, puppyLng, "km");
 
+  // get my current Position
   push();
-  var titlepuppy = "There's no color in my life if my puppy is ";
-  drawingContext.font = "normal 20px Roboto Slab";
-  fill(255);
-  noStroke()
-  text(titlepuppy + Math.round(disAway) + 'Km away...', 70, 70);
-  pop();
 
-
-  push();
-  var puppy = myMap.latLngToPixel(puppyLat, puppyLng);
-  ellipse(puppy.x, puppy.y, 8, 8);
-  noFill();
-  stroke(255, 217, 0);
-  pop();
-
-  push();
-  fill(255, 217, 0);
+  var me = myMap.latLngToPixel(myLoc.latitude, myLoc.longitude);
   noStroke();
-  var me = myMap.latLngToPixel(myPos.latitude, myPos.longitude);
-  ellipse(me.x, me.y, 5, 5);
+  fill(138, 66, 245, 60);
+  ellipse(me.x, me.y, 25);
+  fill(138, 66, 245, 180);
+  ellipse(me.x, me.y, 10);
   pop();
 
+  // extract the data from json file array and graphic the coordinates
   push();
-  stroke(255, 217, 0);
-  strokeWeight(0.8);
-  line(me.x,me.y,puppy.x,puppy.y);
+  var wonders = data.wonders;
+  for (var i = 0; i < data.wonders.length; i++) {
+    var wonders = myMap.latLngToPixel(data.wonders[i].latitude, data.wonders[i].longitude); {
+      stroke("blue");
+      strokeWeight(5)
+      noFill();
+      ellipse(wonders.x, wonders.y, 8);
+    }
+  }
+  pop();
 
+  //the line between me and 7 wonders
+  push();
+  stroke(102, 102, 232, 100);
+  strokeWeight(2);
+  line(me.x, me.y, wonders.x, wonders.y);
+  pop();
+
+  //calculate the distance between previous data
+  push();
+  fill(255);
+  noStroke();
+  rect((width / 8) - 50, (height / 2) + 235, 480, 18);
+  var distance = calcGeoDistance(tLat, tLon, me.x, me.y, "km");
+  var far = "km separete you from ancient seven wonders ";
+  textSize(15);
+  textFont("courier new");
+  fill(48, 6, 125);
+  strokeWeight(0.5);
+  stroke(48, 6, 125);
+  text(Math.round(distance) + far, (width / 8) - 50, (height / 2) + 250);
+  pop();
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
